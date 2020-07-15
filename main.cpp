80cbf247ffc9838266076eb0f80c2e1b23711b13
Mikhail <saratovskikhms@gmail.com>
2020-07-15 18:11:37 +0300

    Fix cmake

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
+#include "cpl_conv.h" // for CPLMalloc()
+#include "gdal_priv.h"
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    cout << "Start Pixel Remover!!!" << endl;
+    const char *pszFilename = "";
+    GDALDataset *poDataset;
+    GDALAllRegister();
+    //    poDataset = (GDALDataset *) GDALOpen(pszFilename, GA_ReadOnly);
     return 0;
 }
